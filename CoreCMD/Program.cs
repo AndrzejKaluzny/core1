249aaabdda7bf632729b0439dff7c06638eed749
Andrzejkaluzny <andrzejkaluzny74@gmail.com>
2020-10-16 08:57:11 +0200

    cmd

---
+++ CoreCMD/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoreCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
